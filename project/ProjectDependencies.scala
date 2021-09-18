aa454839f5ecc2fa01833082d6adb94524cbe127
Scala Steward <me@scala-steward.org>
2021-09-18 07:02:49 +0200

    Update cats-effect to 3.2.9

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -37,7 +37,7 @@ object ProjectDependencies {
   private val effects: Seq[ModuleID] =
     Seq(
       "org.typelevel" %% "cats-core" % "2.6.1",
-      "org.typelevel" %% "cats-effect" % "3.2.8",
+      "org.typelevel" %% "cats-effect" % "3.2.9",
       "org.typelevel" %% "log4cats-slf4j" % "2.1.1",
       "org.typelevel" %% "cats-effect-testing-scalatest" % "1.3.0" % Test
     )
