83ddd8d5b0208a420651d74a7d0fecb3f333f1a0
David Geirola <david.geirola@gmail.com>
2023-06-05 10:48:10 +0200

    Limit RuleRef size

--- core/src/main/scala/erules/RuleRef.scala
+++ core/src/main/scala/erules/RuleRef.scala
@@ -3,18 +3,22 @@ package erules
 import cats.{Eq, Show}
 import cats.kernel.Hash
 
+import java.nio.charset.StandardCharsets
+
 /** A unique reference to a rule.
   */
-final class RuleRef private[erules] (val value: BigInt) extends AnyVal with Serializable
+final class RuleRef private[erules] (val value: BigInt) extends AnyVal with Serializable {
+  override def toString: String = value.toString()
+}
 object RuleRef {
 
   def fromString(value: String): RuleRef = {
-
-    val withoutSpaces: String = value.filter(_ != ' ')
-    val withoutSpacesLen      = withoutSpaces.length
-    val res =
-      s"$withoutSpacesLen${value.length - withoutSpacesLen}${BigInt(withoutSpaces.getBytes)}"
-    new RuleRef(BigInt(res.getBytes.slice(0, 64)))
+    val len         = 8
+    val base        = if (value.isEmpty) "0" else value
+    val bytes       = base.getBytes(StandardCharsets.UTF_8).slice(0, len)
+    val fillerBytes = Array.fill[Byte](len - bytes.length)(0)
+    val filledBytes = bytes ++ fillerBytes
+    new RuleRef(BigInt(filledBytes).abs)
   }
 
   implicit val eq: Eq[RuleRef]     = Eq.by(_.value)
--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -57,6 +57,7 @@ object ProjectDependencies {
     "org.scalactic" %% "scalactic" % "3.2.15" % Test,
     "org.scalatest" %% "scalatest" % "3.2.15" % Test,
     "org.scalameta" %% "munit" % "0.7.29" % Test,
+    "org.scalameta" %% "munit-scalacheck" % "0.7.29" % Test,
     "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test
   )
 
