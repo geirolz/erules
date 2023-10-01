6e0a873f958022db95f8aeae0d545cb878d3915f
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2023-10-01 00:14:18 +0000

    Update cats-effect to 3.5.2

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys.scalaVersion
 object ProjectDependencies {
 
   private val catsVersion       = "2.9.0"
-  private val catsEffectVersion = "3.5.1"
+  private val catsEffectVersion = "3.5.2"
   private val circeVersion      = "0.14.5"
   private val catsXmlVersion    = "0.0.9"
 
