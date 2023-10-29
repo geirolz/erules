90fe9148a832d22cbf7b18fc813f1e61d523380e
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2023-10-29 00:16:20 +0000

    Revert commit(s) c10ee43

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys.scalaVersion
 
 object ProjectDependencies {
 
-  private val catsVersion       = "2.10.0"
+  private val catsVersion       = "2.9.0"
   private val catsEffectVersion = "3.5.1"
   private val circeVersion      = "0.14.5"
   private val catsXmlVersion    = "0.0.9"
