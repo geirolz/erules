d9b929444b4d4dd8b4d89cc8227a50f9eee0b263
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-06-23 00:25:25 +0000

    Update circe-core, circe-generic, ... to 0.14.8

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -5,7 +5,7 @@ object ProjectDependencies {
 
   private val catsVersion       = "2.12.0"
   private val catsEffectVersion = "3.5.4"
-  private val circeVersion      = "0.14.7"
+  private val circeVersion      = "0.14.8"
   private val catsXmlVersion    = "0.0.14"
 
   object Plugins {
